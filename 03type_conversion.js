c049a0f60d50e3669f0d2d587740cd870aa6fd56
Aditya <aditya.vishwabharati@gmail.com>
2024-08-16 15:07:40 +0530

    implicit conversion

---
+++ 03type_conversion.js
@@ -0,0 +1,30 @@
+/*JavaScript handles type conversion in two primary ways:
+
+1. Implicit Conversion (Type Coercion)
+2. Explicit Conversion*/
+
+// 1.  Implicit Conversion (Type Coercion)
+
+// JavaScript often performs automatic type conversion when combining different data types. This is known as type coercion.
+
+// Example 1: String + Number
+// let x = 33;
+// let y = "aditya";
+// let z = x + y;
+// console.log(z); // 33aditya
+
+// Example 2: Number + Boolean
+// let x = 33;
+// let y = true;
+// let z = x + y;
+// console.log(z); // 34
+
+// Example 3: String + Boolean
+// let x = "33";
+// let y = true;
+// let z = x + y;
+// console.log(z); // 33true
+
+// Boolean Context:
+
+
